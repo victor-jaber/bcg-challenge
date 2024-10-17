d6988ff5578f258484f8ca7f50ee532eba1d778c
Victor <victorjaber19@gmail.com>
2024-10-16 23:04:38 -0300

    adding frontend

---
+++ front-end/utils/app/api.ts
@@ -0,0 +1,9 @@
+import { Plugin, PluginID } from '@/types/plugin';
+
+export const getEndpoint = (plugin: Plugin | null) => {
+  if (!plugin) {
+    return 'api/chat';
+  }
+
+  return 'api/chat';
+};
