124c9342236b0f1062bfbad5cfce990a3668d771
Pedro <pedrohaa.silva@gmail.com>
2024-10-20 22:49:54 -0300

    refactor: remove deprecated front-end and back-end implementations

--- front-end/utils/app/api.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin, PluginID } from '@/types/plugin';
-
-export const getEndpoint = (plugin: Plugin | null) => {
-  if (!plugin) {
-    return 'api/chat';
-  }
-
-  return 'api/chat';
-};
